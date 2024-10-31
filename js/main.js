f9593d77464072aeb521e0477e0eddab52b06ac8
teport <sebasdiasa@gmail.com>
2024-10-31 14:23:43 -0600

    act

--- js/main.js
+++ js/main.js
@@ -2,10 +2,12 @@ const nav = document.querySelector("#nav");
 const abrir = document.querySelector("#abrir");
 const cerrar = document.querySelector("#cerrar");
 
-abrir.addEventListener("click", () =>{
+abrir.addEventListener("click", () => {
     nav.classList.add("visible");
-})
+    document.body.classList.add("no-scroll"); // Desactivar scroll
+});
 
-cerrar.addEventListener("click", ()=>{
+cerrar.addEventListener("click", () => {
     nav.classList.remove("visible");
-})
+    document.body.classList.remove("no-scroll"); // Activar scroll
+});
