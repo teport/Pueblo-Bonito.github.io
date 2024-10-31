496c39eb2127ef7265675cef8e8d84f69d2ca5be
teport <sebasdiasa@gmail.com>
2024-10-31 14:32:20 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,22 @@
 const nav = document.querySelector("#nav");
 const abrir = document.querySelector("#abrir");
 const cerrar = document.querySelector("#cerrar");
+const enlaces = document.querySelectorAll(".nav-list a");
 
 abrir.addEventListener("click", () => {
     nav.classList.add("visible");
-    document.body.classList.add("no-scroll"); // Desactivar scroll
+    document.body.classList.add("no-scroll");
 });
 
 cerrar.addEventListener("click", () => {
     nav.classList.remove("visible");
-    document.body.classList.remove("no-scroll"); // Activar scroll
+    document.body.classList.remove("no-scroll");
+});
+
+
+enlaces.forEach(enlace => {
+    enlace.addEventListener("click", () => {
+        nav.classList.remove("visible");
+        document.body.classList.remove("no-scroll");
+    });
 });
